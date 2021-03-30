86e3fb9dfaa7f4f8912bafa6ec851c6f85986227
rubensrvsc <Rubensspfc100@gmail.com>
2021-03-30 08:54:32 -0300

    model: modifing Voting model to Eleitor model and add Vote model

--- voting/models.py
+++ voting/models.py
@@ -3,5 +3,10 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Voting(models.Model):
+class Vote(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=300)
+
+class Eleitor(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
+    vote = models.ForeignKey(Vote,related_name="user_vote",on_delete=models.CASCADE)
